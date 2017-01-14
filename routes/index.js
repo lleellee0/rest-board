8b54a64e58b1eae0d582fd59b5eb0d57bc00eca2
Jun <lleellee013@gmail.com>
2017-01-14 21:09:05 +0900

    Mobirise로 틀 만들기 // PJAX로 페이지 이동(header, footer의 ejs를 조건부로 include)
    ref : http://ejs.co/

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {req: req});
+});
+
+router.get('/board', function(req, res, next) {
+  res.render('board', {req: req});
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('users', {req: req});
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {req: req});
 });
 
 module.exports = router;
