223b954848e257552a0bfd8c989edec4edf2571f
Jun <lleellee013@gmail.com>
2017-01-26 03:01:24 +0900

    로그인 체크 후 토큰 localStorage에 저장 구현
    1. ajax url 도메인 부분 바꿀 수 있도록 구현
    2. 주석 추가
    3. localStorage에 json객체 저장
    4. 서버쪽 로그인 실패시 403에러 반환
    5. 성공시 반환되는 JSON에 password 삭제
    6. value를 가져오기 위한 id 추가
    
    TODO
     - LOGIN 버튼 없어지게 한 후 LOGOUT 버튼으로 대체
     - 페이지 로딩 시 token의 exp를 검사해서 유효한 상태인지 체크 => LOGIN 혹은 LOGOUT으로 버튼 생성

--- public/assets/custom.js
+++ public/assets/custom.js
@@ -1,3 +1,4 @@
+const apiServerAddress = "";
 var nonce = "1234567890!@#$%^&*";
 
 $('body').on('click', function(event) {
@@ -21,6 +22,8 @@ $('.modalBackground').on('click', function(event) {
   event.stopPropagation();
 });
 
+// 로그인 모달과 로딩 모달에 대한 보여주기/감추기
+// DOM을 다시 탐색하지 않도록 const로 저장해둠.
 const $modalBackground = $('.modalBackground');
 const $fixedModal = $('#fixedModal');
 const $loadingDiv = $('.loadingDivWrapper');
@@ -49,6 +52,7 @@ const hideLoadingDiv = function() {
   $loadingDiv.addClass('opacityZero');
 }
 
+// blur 클래스에는 filter: blur(5px); 속성이 걸려있음.(custom.css 참고)
 const addBlurAtDiv = function($div) {
   $div.addClass('blur');
 }
@@ -57,6 +61,8 @@ const removeBlurAtDiv = function($div) {
   $div.removeClass('blur');
 }
 
+
+// 비밀번호와 비밀번호 확인의 일치여부 확인
 const password = document.getElementById("signup_password");
 const confirmPassword = document.getElementById("signup_confirm_password");
 
@@ -71,6 +77,9 @@ const validatePassword = function() {
 password.onchange = validatePassword;
 confirmPassword.onkeyup = validatePassword;
 
+
+// Signup 창의 CREATE 버튼이 클릭되었을 때 실행됨.
+// 아래의 processLogin과 비슷한 기능을 하며 ajax 호출하는 부분만 다름
 const processSignup = function() {
   showLoadingDiv();
   addBlurAtDiv($fixedModal);
@@ -78,6 +87,7 @@ const processSignup = function() {
   return false;
 }
 
+// login 창의 LOGIN 버튼이 클릭되었을 때 실행됨.
 const processLogin = function() {
   showLoadingDiv();
   addBlurAtDiv($fixedModal);
@@ -90,5 +100,22 @@ const sendSignupAjax = function() {
 }
 
 const sendLoginAjax = function() {
+  $.ajax({
+    url: apiServerAddress + '/users/login-check?user_id=' +
+      $('#login_id').val() + '&password=' + $('#login_password').val() + '&nonce=' + nonce,
+    method: 'GET',
+    success: function(data) {
+      removeBlurAtDiv($fixedModal);
+      hideLoadingDiv();
+      hideLoginModal();
+      console.log(data);
+      // 1. 우측상단 LOGIN 버튼 변경
+      // 2. token을 스토리지에 저장 (완료)
 
+      localStorage.setItem('session', JSON.stringify(data));
+    },
+    error: function(xhr, status, err) {
+      console.log(err);
+    }
+  })
 }
--- routes/users/users.controller.js
+++ routes/users/users.controller.js
@@ -14,9 +14,16 @@ exports.loginCheck = function(req, res, next) {
   nonce = crypto.createHash('sha256').update(nonce).digest('hex');
 
   connection.query('SELECT id, password, user_id, nickname FROM users WHERE user_id=? AND password=password(?)', [user_id, password + salt], function(err, result) {
+    if(result[0] === undefined)
+      res.status(403).end();
+
     let row = result[0];
     row.token = crypto.createHash('sha256').update(nonce + row.password).digest('hex');
     row.token_exp = Math.floor(Date.now() / 1000) + (60 * 60);
+
+    // DB에 저장된 해시화 비밀번호는 전달하지 않음.
+    row.password = undefined;
+
     connection.query('INSERT INTO token (users_id, token, exp) VALUES (?, ?, ?)', [row.id, row.token, row.token_exp], function(err, result) {
       if(err) throw err;
       res.json(row);
